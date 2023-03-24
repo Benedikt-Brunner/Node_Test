ebfe3a041c77d2fb2287ebe3c67cb51cce7405c5
Benedikt Brunner <122370755+Benedikt-Brunner@users.noreply.github.com>
2023-03-24 23:36:49 +0100

    little changes

--- SELECT tournaments.sql
+++
@@ -1,4 +0,0 @@
-SELECT tournaments.Name, tournamentresults.placement,players.name
-FROM tournamentresults
-INNER JOIN tournaments ON tournamentresults.tournament_id=tournaments.id
-INNER JOIN players ON tournamentresults.player_id=players.id;
